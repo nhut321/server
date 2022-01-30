21278b46f0c63008315c1c4040fded06548a73d5
nhut321 <znhut12z@gmail.com>
2022-01-31 00:16:27 +0700

    update auth and index router

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 function auth(req,res,next) {
-	const token =  req.headers["authorization"]
+	const authHeader = req.header('Authorization')
+	const token = authHeader && authHeader.split(' ')[1]
 	if(!token) {
-		res.json('Token is not found!')
+		return res.status(401).json('Access token not found!')
 	}
 	if(token) {
 		const newToken = token.split(' ')[1]
 		try {
 			const decoded = jwt.verify(newToken, process.env.SECRET_KEY)
 			req.user = decoded
-			next()
+			next() 
 
 		} catch(err) {
-			res.status(400).json({
+			return res.status(400).json({
 				success: false,
 				message: 'Invalid token'
 			})
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,34 @@ const auth = require('./auth')
 const musicRouter = require('./musicRouter')
 const spotifyRouter = require('./spotifyRouter')
 const postRouter = require('./postRouter')
+const User = require('../models/User')
 
 function routes(app) {
 	app.use('/post', postRouter)
 	app.use('/spotify', spotifyRouter)
 	app.use('/music', musicRouter)
 	app.use('/auth', authRouter)
-	app.get('/', auth, (req,res) => {
-		res.status(200).json({
-			isAuth: true,
-			message: 'Authorizated'
-		})
+	app.get('/', auth, async (req,res) => {
+		const user = await User.findOne({req.user})
+		if (!user) return res.status(400).json('User not found!')
+
+		try {
+			res.status(200).json({
+				success: true,
+				message: 'Authorizated',
+				user
+			})
+		} catch(err) {
+			res.status(401).json({
+				success: false,
+				message: 'User not authorizated'
+			})
+		}
+		//  res.status(200).json({
+		// 	isAuth: true,
+		// 	message: 'Authorizated',
+		// 	user: req.user
+		// })
 	})
 }
 
